489ea393908548704d8c01feaa494d1c10138a2c
HNaguib <mr.si7s@hotmail.com>
2021-06-16 05:09:02 +0200

    Postgres DB config

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,9 +3,10 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
-    ordering=['id']
-    list_display=['email','name']
+    ordering = ['id']
+    list_display = ['email','name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
@@ -24,9 +25,10 @@ class UserAdmin(BaseUserAdmin):
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
+            'classes': ('wide', ),
             'fields': ('email', 'password1', 'password2'),
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
